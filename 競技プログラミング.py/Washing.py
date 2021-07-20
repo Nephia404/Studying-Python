d6b3094dc149b13c2f70a1b16614c3920a8bb5d4
Nephia <yuukisana0514@gmail.com>
2021-07-20 20:35:00 +0900

    test

---
+++ 競技プログラミング.py/Washing.py
@@ -0,0 +1,12 @@
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+
+Time = 0
+Count = 0
+
+for i in range(N):
+    while (A[i] > K):
+        A[i] = A[i]-1
+        Count = Count + 1
+
+print(Count)
